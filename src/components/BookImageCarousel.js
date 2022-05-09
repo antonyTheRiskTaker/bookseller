bcd63149da0814799ca10697f105822490577722
Antony Lo <mrmikadox@gmail.com>
2022-05-09 19:32:27 +0800

    download and add two russian tank memes

--- src/components/BookImageCarousel.js
+++ src/components/BookImageCarousel.js
@@ -1,6 +1,8 @@
 import Carousel from 'react-bootstrap/Carousel';
 
 import stolenRussianTank1 from '../images/stolen-russian-tank-1.jpg';
+import stolenRussianTank2 from '../images/stolen-russian-tank-2.webp';
+import stolenRussianTank3 from '../images/stolen-russian-tank-3.jpg';
 
 export default function BookImageCarousel() {
   return (
@@ -15,12 +17,14 @@ export default function BookImageCarousel() {
         </Carousel.Item>
         <Carousel.Item>
           <img
+            src={stolenRussianTank2}
             className='d-block w-100'
             alt='2nd slide'
           />
         </Carousel.Item>
         <Carousel.Item>
           <img
+            src={stolenRussianTank3}
             className='d-block w-100'
             alt='3rd slide'
           />
